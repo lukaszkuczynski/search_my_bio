ab6f65d6c684e032c38e98de4e296d44b8fadf9c
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-07-13 04:30:36 +0800

    Indexing my projects in Elasticsearch!

--- indexmyprojects.py
+++ indexmyprojects.py
@@ -1,17 +1,22 @@
 import yaml
 from elasticsearch import Elasticsearch
 
+def projects_from_file(fname):
+    with open(fname) as f:
+        doc = yaml.load(f)
+        commercial = doc['Projects']['Commercial']
+        for project in commercial.keys():
+            yield (project, commercial[project])
+        private = doc['Projects']['Private']
+        for project in private.keys():
+            yield (project, private[project])
+
 fname = "projects.yml"
-with open(fname) as f:
-    doc = yaml.load(f)
-    commercial = doc['Projects']['Commercial']
-    print("--Listing commercial projects--")
-    for project in commercial.keys():
-        print(project)
-    private = doc['Projects']['Private']
-    print("--Listing side-projects--")
-    for project in private.keys():
-        print(project)
 
 es = Elasticsearch()
-print(es.info())
+for project_name_and_value in projects_from_file(fname):
+    project_name = project_name_and_value[0]
+    project_details = project_name_and_value[1]
+    es.index(index="searchmybio_luke", doc_type='project', id=project_name, body=project_details)
+
+
