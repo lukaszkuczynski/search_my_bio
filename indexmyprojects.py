c7a6d220638c0db09d4118a23f309a0b18dcf58b
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-07-13 04:11:00 +0800

    Yaml parsing

---
+++ indexmyprojects.py
@@ -0,0 +1,14 @@
+import yaml
+
+fname = "projects.yml"
+with open(fname) as f:
+    doc = yaml.load(f)
+    commercial = doc['Projects']['Commercial']
+    print("--Listing commercial projects--")
+    for project in commercial.keys():
+        print(project)
+    private = doc['Projects']['Private']
+    print("--Listing side-projects--")
+    for project in private.keys():
+        print(project)
+
