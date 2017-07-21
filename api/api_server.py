6d6a708c9e17e4366b723f4f731e91185d2fcf7a
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-07-21 18:43:16 +0200

    Web app calls API

--- api/api_server.py
+++ api/api_server.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from api.dao_elasticsearch import ElasticDao
 from api.response_converter import EsResponseConverter
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app)
+
 converter = EsResponseConverter()
 
 elastic_config = {
@@ -19,6 +23,7 @@ def hello():
 @app.route("/api/search")
 def api_search():
     queried_text = request.args.get("q")
+    print('query is %s' % queried_text)
     response = dao.query_all_fields(queried_text)
     simple_response = converter.response_to_simple_hits(response)
     return jsonify(simple_response)
