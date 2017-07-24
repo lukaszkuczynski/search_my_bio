e624befec8c746c774a5b6293e5f57da3af37dd0
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-07-24 18:03:40 +0200

    Removing index before creation

--- prepare_index.py
+++ prepare_index.py
@@ -2,6 +2,8 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
+
+es.indices.delete("searchmybio_luke")
 print("Creating index")
 res = es.indices.create("searchmybio_luke")
 print("Result %s" % res)
