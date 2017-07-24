7a7993ce4771467a8f7d5f0840d7c2318eec9285
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-07-24 21:48:06 +0200

    static page prepared for CV printing

--- web/web_server.py
+++ web/web_server.py
@@ -8,3 +8,8 @@ app.debug = True
 def hello():
     message = "This is front page"
     return render_template("index.html", message = message)
+
+
+@app.route("/cv")
+def cv():
+    return render_template("static_cv.html")
