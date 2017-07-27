5a2cd1b0077b3d3d3e6db778a209b93433c41dc0
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-07-27 19:03:29 +0200

    Indexing with regard is it commercial or private project

--- indexmyprojects.py
+++ indexmyprojects.py
@@ -8,19 +8,21 @@ def projects_from_file(fname):
         doc = yaml.load(f)
         commercial = doc['Projects']['Commercial']
         for project in commercial.keys():
-            yield (project, commercial[project])
+            yield ('commercial', project, commercial[project])
         private = doc['Projects']['Private']
         for project in private.keys():
-            yield (project, private[project])
+            yield ('private', project, private[project])
 
 fname = "life_tasks.yml"
 
 elastic_hosts = load_config('elastic-config.yml')
 es = Elasticsearch(hosts=elastic_hosts, verify_certs=False)
-for project_name_and_value in projects_from_file(fname):
-    project_name = project_name_and_value[0]
-    project_details = project_name_and_value[1]
+for project_tuple in projects_from_file(fname):
+    commercial_or_private = project_tuple[0]
+    project_name = project_tuple[1]
+    project_details = project_tuple[2]
     project_details['title'] = project_name
+    project_details['type'] = commercial_or_private
     es.index(index="searchmybio_luke", doc_type='project', id=project_name, body=project_details)
 
 
